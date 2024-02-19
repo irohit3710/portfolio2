51cc22bd60ea7c549571b5566b1968acabc5aec1
Rohit Panwar <133122171+irohit3710@users.noreply.github.com>
2024-02-19 14:26:03 +0530

    install hotjar

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import Hotjar from '@hotjar/browser';
 
+const siteId = 3871383;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
