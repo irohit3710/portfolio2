f312cae4dc0d702029a1999dba15287af6f4c0f0
Rohit Panwar <133122171+irohit3710@users.noreply.github.com>
2024-02-19 15:24:47 +0530

    hotjar changes 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 import Banner from './components/Banner/Banner';
 import Features from './components/Features/Features';
@@ -6,9 +7,17 @@ import FooterBottom from './components/Footer/FooterBottom';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Contact from './components/contact/Contact.jsx';
 import Project from './components/projects/Project';
+import Hotjar from '@hotjar/browser';
+
+const siteId = 3871383;
+const hotjarVersion = 6;
+
 
 
 function App() {
+    useEffect(()=>{
+      Hotjar.init(siteId, hotjarVersion);
+    },[])
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText ">
       <div className=' max-w-screen-2xl mx-auto'>
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,14 @@
 import React, { useEffect } from 'react';
-import { hotjar } from 'react-hotjar';
-import mixpanel from 'mixpanel-browser';
-import { getHotjarMixpanelDetails } from '../api/projects';
-import useRequestApi from '../hooks/useRequestApi';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const _ = require('lodash');
 
-export const App = () => {
-  const getDetailsApi = useRequestApi(getHotjarMixpanelDetails);
-  useEffect(() => {
-    handleHotjarMixpanelDetails();
-  }, []);
-
-  const handleHotjarMixpanelDetails = async () => {
-    await getDetailsApi.requestDetails();
-    if (!_.isEmpty(getDetailsApi.results.data)) {
-      hotjar.initialize(getDetailsApi.results.data.hotjar_site_id, getDetailsApi.results.data.hotjar_snippet_version);
-      mixpanel.init(getDetailsApi.results.data.mixpanelProjectToken);
-    }
-  }
-  return (
-    <Grid>App file content goes here</Grid>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  
+);
